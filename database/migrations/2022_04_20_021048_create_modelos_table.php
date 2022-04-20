c7e0bd61a1b15e0d03f309a0b15b2dca3c001931
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-04-20 00:05:47 -0300

    [Feature] Added Modelo classes

---
+++ database/migrations/2022_04_20_021048_create_modelos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateModelosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('modelos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('marca_id')->constrained('marcas')->cascadeOnDelete();
+            $table->string('nome', 30);
+            $table->string('imagem', 100);
+            $table->integer('numero_portas');
+            $table->integer('lugares');
+            $table->boolean('air_bag');
+            $table->boolean('abs');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('modelos');
+    }
+}
