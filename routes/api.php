59d849026893d7254da3cc73e3d653181604b1bd
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-04-21 00:40:30 -0300

    [Feature] Added resource routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('cliente', 'ClienteController');
+Route::apiResource('marca', 'MarcaController');
+Route::apiResource('modelo', 'ModeloController');
+Route::apiResource('carro', 'CarroController');
+Route::apiResource('locacao', 'LocacaoController');
