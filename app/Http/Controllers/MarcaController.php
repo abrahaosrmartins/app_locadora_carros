e50f31f9d621c0ab7331b10e9e2a26e4b941d580
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-04-21 00:47:48 -0300

    [Refactor] Updated MarcaController Methods

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateMarcaRequest;
+use App\Http\Requests\UpdateMarcaRequest;
 use App\Models\Marca;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class MarcaController extends Controller
 {
@@ -23,58 +28,77 @@ class MarcaController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
      * Display a listing of the resource.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      * @return JsonResponse
      */
-    public function store(Request $request)
     public function index(): JsonResponse
     {
-        //
         $marcas = $this->marca->all();
         return response()->json($marcas, 200);
     }
 
     /**
-     * Display the specified resource.
+     * Store a newly created resource in storage.
      *
-     * @param  \App\Models\Marca  $marca
-     * @return \Illuminate\Http\Response
+     * @param CreateMarcaRequest $createMarcaRequest
+     * @return JsonResponse
      */
-    public function show(Marca $marca)
+    public function store(CreateMarcaRequest $createMarcaRequest): JsonResponse
     {
-        //
+        $marca = $this->marca->create($createMarcaRequest->validated());
+
+        return response()->json($marca, 201);
     }
 
     /**
+     * Display the specified resource.
      *
+     * @param int $id
+     * @return JsonResponse
      */
+    public function show(int $id)
     {
+        $marca = $this->marca->find($id);
+
+        if ($marca === null) {
+            return response()->json(['erro' => 'Registro inexistente no banco de dados'], 404);
+        }
+        return response()->json($marca, 200);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Marca  $marca
-     * @return \Illuminate\Http\Response
+     * @param UpdateMarcaRequest $updateMarcaRequest
+     * @param int $id
+     * @return JsonResponse
      */
-    public function update(Request $request, Marca $marca)
+    public function update(UpdateMarcaRequest $updateMarcaRequest, int $id)
     {
-        //
+        $marca = $this->marca->find($id);
+        if ($marca === null) {
+            return response()->json(['erro' => 'Registro inexistente no banco de dados'], 404);
+        }
+        $marca->update($updateMarcaRequest->validated());
+
+        return response()->json($marca, 200);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Marca  $marca
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return JsonResponse
      */
-    public function destroy(Marca $marca)
+    public function destroy(int $id)
     {
-        //
+        $marca = $this->marca->find($id);
+        if ($marca === null) {
+            return response()->json(['erro' => 'Registro inexistente no banco de dados'], 404);
+        }
+        $marca->delete();
+
+        return response()->json(['msg' => 'Marca removida com sucesso'], 200);
     }
 }
