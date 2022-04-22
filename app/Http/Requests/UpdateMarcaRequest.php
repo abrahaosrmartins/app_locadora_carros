3a70043b562ea80a5570b5d1440627fe32f8bf96
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-04-22 16:40:34 -0300

    [Style] Added function return types

--- app/Http/Requests/UpdateMarcaRequest.php
+++ app/Http/Requests/UpdateMarcaRequest.php
@@ -11,7 +11,7 @@ class UpdateMarcaRequest extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
@@ -21,7 +21,7 @@ class UpdateMarcaRequest extends FormRequest
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
         return [
             'nome' => 'sometimes|unique:marcas,nome,' . $this->route('marca'),
