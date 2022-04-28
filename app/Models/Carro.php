04b8e24337698b0f9840103d009ca6836fde61f8
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-04-28 01:19:03 -0300

    [Refactor] Changed Models
    - Carro
    - Cliente
    - Locacao

--- app/Models/Carro.php
+++ app/Models/Carro.php
@@ -7,9 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * @method create(array $array)
+ * @method find($id)
+ */
 class Carro extends Model
 {
     use HasFactory;
+    protected $fillable = ['modelo_id', 'placa', 'disponivel', 'km'];
+
+    public function rules() {
+        return [
+            'modelo_id' => 'exists:modelos,id',
+            'placa' => 'required',
+            'disponivel' => 'required',
+            'km' => 'required'
+        ];
+    }
 
     /**
      * Creates relationship with Modelo`s model
