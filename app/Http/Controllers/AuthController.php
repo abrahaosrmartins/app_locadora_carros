3350f0b7fcbea8f0b65ed560927678066ef68239
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-05-01 01:28:14 -0300

    [Feature] Added login method

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Receives auth params and returns a JWT
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function login(Request $request)
+    {
+        // método auth retorna o token de autorização
+        $token = auth('api')->attempt($request->all(['email', 'password']));
+
+        if ($token) { // usuário autenticado com sucesso
+            return response()->json(['token' => $token], 200);
+        } else { // usuário ou senha inválidos
+            return response()->json(['erro' => 'Usuário ou senha inválidos'], 403);
+        }
+    }
+
+}
