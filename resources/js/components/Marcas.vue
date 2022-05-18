2d798b6b2239cca43b0fce88cc42810b04d9d2ef
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-05-18 17:10:56 -0300

    [Feature] Added Table component

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -2,13 +2,18 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-md-8">
-        <div class="card">
-          <div class="card-header">Marcas</div>
 
-          <div class="card-body">
-            <p>Registros de marcas</p>
-          </div>
-        </div>
+        <!-- início do card de listagem de marcas -->
+        <card-component titulo="Relação de Marcas">
+          <template v-slot:conteudo>
+            <table-component></table-component>
+          </template>
+          <template v-slot:rodape>
+            <button type="button" class="btn btn-primary btn-sm float-right">Adicionar</button>
+          </template>
+        </card-component>
+        <!-- final do card de listagem de marcas -->
+
       </div>
     </div>
   </div>
