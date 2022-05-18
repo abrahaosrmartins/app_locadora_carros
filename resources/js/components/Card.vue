435a0a33ae4e40a567b52051122cad70c51b135a
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-05-18 20:20:42 -0300

    [Style] Updated Vue components indentation

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="card mb-3">
-    <div class="card-header">{{ titulo }}</div>
+    <div class="card mb-3">
+        <div class="card-header">{{ titulo }}</div>
 
-    <div class="card-body">
-      <slot name="conteudo"></slot>
+        <div class="card-body">
+            <slot name="conteudo"></slot>
+        </div>
+
+        <div class="card-footer">
+            <slot name="rodape"></slot>
+        </div>
+
     </div>
-
-    <div class="card-footer">
-      <slot name="rodape"></slot>
-    </div>
-
-  </div>
 </template>
 
 <script>
 export default {
-  props: ['titulo']
+    props: ['titulo']
 }
 </script>
