82b71e157d34e0391192aa579319b08046fccdd3
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-05-18 22:00:31 -0300

    [Refactor] Removed axios dependency from alert component

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -97,7 +97,7 @@ export default {
             nomeMarca: '',
             arquivoImagem: [],
             transacaoStatus: '',
-            transacaoDetalhes: []
+            transacaoDetalhes: {}
         }
     },
     methods: {
@@ -124,12 +124,18 @@ export default {
             axios.post(this.urlBase, formData, config)
                 .then(response => {
                     this.transacaoStatus = 'adicionado'
-                    this.transacaoDetalhes = response
+                    this.transacaoDetalhes = {
+                        mensagem: 'ID do registro: ' + response.data.id
+                    }
+
                     console.log(response)
                 })
                 .catch(errors => {
                     this.transacaoStatus = 'erro'
-                    this.transacaoDetalhes = errors.response
+                    this.transacaoDetalhes = {
+                        mensagem: errors.response.data.message
+                        dados: errors.response.data.errors
+                    }
                     // errors.response.data.message
                 })
         }
