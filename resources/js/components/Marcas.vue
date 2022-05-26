560468f5c136111fb9ab739f3c18c358ff11c41a
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-05-25 22:18:03 -0300

    [Feature] Added update request to component

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -40,7 +40,7 @@
                         <table-component
                             :dados="marcas.data"
                             :visualizar="{visivel: true, dataToggle: 'modal', dataTarget: '#modalMarcaVisualizar'}"
-                            :atualizar="true"
+                            :atualizar="{visivel: true, dataToggle: 'modal', dataTarget: '#modalMarcaAtualizar'}"
                             :remover="{visivel: true, dataToggle: 'modal', dataTarget: '#modalMarcaRemover'}"
                             :titulos="{
                                 id: {titulo: 'ID', tipo: 'texto'},
@@ -167,6 +167,40 @@
             </template>
         </modal-component>
         <!-- fim do modal de remoção de marca -->
+
+        <!-- início do modal de atualização de marca -->
+        <modal-component id="modalMarcaAtualizar" titulo="Atualizar marca">
+
+            <template v-slot:alertas>
+            </template>
+
+            <template v-slot:conteudo>
+                <div class="form-group">
+                    <input-container-component titulo="Nome da marca" id="atualizarNome" id-help="atualizarNomeHelp"
+                                               texto-ajuda="Informe o nome da marca">
+                        <input type="text" class="form-control" id="atualizarNome" aria-describedby="atualizarNomeHelp"
+                               placeholder="Nome da marca" v-model="$store.state.item.nome">
+                    </input-container-component>
+                </div>
+
+                <div class="form-group">
+                    <input-container-component titulo="Imagem" id="atualizarImagem" id-help="atualizarImagemHelp"
+                                               texto-ajuda="Selecione uma imagem no formato PNG">
+                        <input type="file" class="form-control-file" id="atualizarImagem"
+                               aria-describedby="atualizarImagemHelp" placeholder="Selecione uma imagem"
+                               @change="carregarImagem($event)">
+                    </input-container-component>
+                </div>
+
+                {{ $store.state.item }}
+            </template>
+
+            <template v-slot:rodape>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                <button type="button" class="btn btn-primary" @click="atualizar()">Atualizar</button>
+            </template>
+        </modal-component>
+        <!-- fim do modal de inclusão de marca -->
     </div>
 </template>
 
@@ -202,6 +236,37 @@ export default {
         }
     },
     methods: {
+        atualizar() {
+
+            let formData = new FormData();
+            formData.append('_method', 'patch')
+            formData.append('nome', this.$store.state.item.nome)
+
+            if (this.arquivoImagem[0]) {
+                formData.append('imagem', this.arquivoImagem[0])
+            }
+
+            let url = this.urlBase + '/' + this.$store.state.item.id
+
+            let config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Accept': 'application/json',
+                    'Autorization': this.token
+                }
+            }
+
+            axios.post(url, formData, config)
+                .then(response => {
+                    console.log('Atualizado', response)
+                    //limpar o campo de seleção de arquivos
+                    atualizarImagem.value = ''
+                    this.carregarLista()
+                })
+                .catch(errors => {
+                    console.log('Erro de atualização', errors.response)
+                })
+        },
         remover() {
             let confirmacao = confirm('Tem certeza que deseja remover esse registro?')
 
--- resources/js/components/Table.vue
+++ resources/js/components/Table.vue
@@ -4,7 +4,7 @@
             <thead>
             <tr>
                 <th scope="col" v-for="t, key in titulos" :key="key">{{ t.titulo }}</th>
-                <th v-if="visualizar.visivel || atualizar || remover.visivel"></th>
+                <th v-if="visualizar.visivel || atualizar.visivel || remover.visivel"></th>
             </tr>
             </thead>
             <tbody>
@@ -18,12 +18,15 @@
                             <img :src="'/storage/'+valor" width="30" height="30">
                         </span>
                 </td>
-                <td v-if="visualizar.visivel || atualizar || remover.visivel">
+                <td v-if="visualizar.visivel || atualizar.visivel || remover.visivel">
                     <button v-if="visualizar.visivel" class="btn btn-outline-primary btn-sm"
                             :data-toggle="visualizar.dataToggle" :data-target="visualizar.dataTarget"
                             @click="setStore(obj)">Visualizar
                     </button>
-                    <button v-if="atualizar" class="btn btn-outline-primary btn-sm">Atualizar</button>
+                    <button v-if="atualizar.visivel" class="btn btn-outline-primary btn-sm"
+                            :data-toggle="atualizar.dataToggle" :data-target="atualizar.dataTarget"
+                            @click="setStore(obj)">Atualizar
+                    </button>
                     <button v-if="remover.visivel" class="btn btn-outline-danger btn-sm"
                             :data-toggle="remover.dataToggle" :data-target="remover.dataTarget" @click="setStore(obj)">
                         Remover
@@ -40,6 +43,8 @@ export default {
     props: ['dados', 'titulos', 'atualizar', 'visualizar', 'remover'],
     methods: {
         setStore(obj) {
+            this.$store.state.transacao.status = ''
+            this.$store.state.transacao.mensagem = ''
             this.$store.state.item = obj
         }
     },
