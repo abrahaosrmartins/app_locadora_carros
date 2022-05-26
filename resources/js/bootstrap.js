6244b62ca9111da098c6fa5f9aa374beed062449
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-05-25 23:03:00 -0300

    [Chore] Added headers to interceptors

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,5 @@
+const {default: axios} = require('axios');
+
 window._ = require('lodash');
 
 /**
@@ -11,7 +13,8 @@ try {
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap');
-} catch (e) {}
+} catch (e) {
+}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -43,23 +46,37 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /* interceptar os requests da aplicação */
 axios.interceptors.request.use(
     config => {
-        console.log(config)
+
+        //deinifir para todas as requisições os parâmetros de accept e autorization
+        config.headers['Accept'] = 'application/json'
+
+        //recuperando o token de autorização dos cookies
+        let token = document.cookie.split(';').find(indice => {
+            return indice.includes('token=')
+        })
+
+        token = token.split('=')[1]
+        token = 'Bearer ' + token
+
+        config.headers.Authorization = token
+
+        console.log('Interceptando o request antes do envio', config)
         return config
     },
     error => {
-        console.log(error)
+        console.log('Erro na requisição: ', error)
         return Promise.reject(error)
     }
 )
 
-/* interceptar os responses da requisição */
+/* interceptar os responses da aplicação */
 axios.interceptors.response.use(
     response => {
-        console.log(response)
+        console.log('Interceptando a resposta antes da aplicação', response)
         return response
     },
     error => {
-        console.log(error)
+        console.log('Erro na resposta: ', error)
         return Promise.reject(error)
     }
 )
