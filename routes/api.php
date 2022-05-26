17ea431bbc56de2b7ff8a418e16034fd9072e794
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-05-25 23:35:20 -0300

    [Chore] Added jwt refresh

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::prefix('v1')->middleware(['jwt.auth'])->group(function () {
     Route::post('me', 'AuthController@me');
-    Route::post('refresh', 'AuthController@refresh');
     Route::post('logout', 'AuthController@logout');
     Route::apiResource('cliente', 'ClienteController');
     Route::apiResource('marca', 'MarcaController');
@@ -30,3 +29,5 @@ Route::prefix('v1')->middleware(['jwt.auth'])->group(function () {
 });
 
 Route::post('login', 'AuthController@login');
+Route::post('refresh', 'AuthController@refresh');
+
