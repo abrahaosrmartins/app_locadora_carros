4e7892e026a63b7ea2451581d75bee691b26fefc
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-05-25 21:50:23 -0300

    [Feature] Added delete request with Axios

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -143,8 +143,14 @@
 
         <!-- início do modal de remoção de marca -->
         <modal-component id="modalMarcaRemover" titulo="Remover marca">
-            <template v-slot:alertas></template>
-            <template v-slot:conteudo>
+            <template v-slot:alertas>
+                <alert-component tipo="success" titulo="Transação realizada com sucesso"
+                                 :detalhes="$store.state.transacao"
+                                 v-if="$store.state.transacao.status == 'sucesso'"></alert-component>
+                <alert-component tipo="danger" titulo="Erro na transação" :detalhes="$store.state.transacao"
+                                 v-if="$store.state.transacao.status == 'erro'"></alert-component>
+            </template>
+            <template v-slot:conteudo v-if="$store.state.transacao.status != 'sucesso'">
                 <input-container-component titulo="ID">
                     <input type="text" class="form-control" :value="$store.state.item.id" disabled>
                 </input-container-component>
@@ -155,6 +161,9 @@
             </template>
             <template v-slot:rodape>
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                <button type="button" class="btn btn-danger" @click="remover()"
+                        v-if="$store.state.transacao.status != 'sucesso'">Remover
+                </button>
             </template>
         </modal-component>
         <!-- fim do modal de remoção de marca -->
@@ -193,6 +202,37 @@ export default {
         }
     },
     methods: {
+        remover() {
+            let confirmacao = confirm('Tem certeza que deseja remover esse registro?')
+
+            if (!confirmacao) {
+                return false;
+            }
+
+            let formData = new FormData();
+            formData.append('_method', 'delete')
+
+            let config = {
+                headers: {
+                    'Accept': 'application/json',
+                    'Autorization': this.token
+                }
+            }
+
+            let url = this.urlBase + '/' + this.$store.state.item.id
+
+            axios.post(url, formData, config)
+                .then(response => {
+                    this.$store.state.transacao.status = 'sucesso'
+                    this.$store.state.transacao.mensagem = response.data.msg
+                    this.carregarLista()
+                })
+                .catch(errors => {
+                    this.$store.state.transacao.status = 'erro'
+                    this.$store.state.transacao.mensagem = errors.response.data.erro
+                })
+
+        },
         pesquisar() {
             //console.log(this.busca)
 
