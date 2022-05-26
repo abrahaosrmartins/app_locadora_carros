face7e637a66e29e8f3f4e8fc4fd8abb7a96c780
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-05-25 22:51:26 -0300

    [Chore] Added Axios request interceptor

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -39,3 +39,15 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+/* interceptar os requests da aplicação */
+axios.interceptors.request.use(
+    config => {
+        console.log(config)
+        return config
+    },
+    error => {
+        console.log(error)
+        return Promise.reject(error)
+    }
+)
